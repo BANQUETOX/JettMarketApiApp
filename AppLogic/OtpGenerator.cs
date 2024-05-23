f003c1d7849e1de6e62ea0f634887b0ec1d14af5
fmenas <banquetox@gmail.com>
2024-05-23 09:55:18 -0600

    Create otp generator class

---
+++ AppLogic/OtpGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic
+{
+    public class OtpGenerator
+    {
+        public static string GenerateOtp()
+        {
+           string otp = "otpCode";
+           return otp;
+            
+        }
+    }
+}
