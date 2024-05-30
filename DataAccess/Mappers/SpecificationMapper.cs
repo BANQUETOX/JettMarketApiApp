c2ad2c634c1272edf9c9a7538b814c42815899a8
Banquetox <banquetox@gmail.com>
2024-05-30 08:42:03 -0600

    start specification mapper

---
+++ DataAccess/Mappers/SpecificationMapper.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using DataAccess.Dao;
+using DTO.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Mappers
+{
+    public class SpecificationMapper
+    {
+        internal SqlDao sqlDao = SqlDao.GetInstance();
+
+        public int CreateSpecification(InputSpecification specification)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@name",specification.name);
+            parameters.Add("@value",specification.value);
+            SqlOperation operation = new SqlOperation();
+            operation.procedureName = "SP_CREATE_SPECIFICATION";
+            operation.parameters = parameters;
+        }
+    }
+}
