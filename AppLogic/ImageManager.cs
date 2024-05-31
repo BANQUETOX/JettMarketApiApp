af87b6b938f5fdc24cee20fc0296e68e4530c698
Banquetox <banquetox@gmail.com>
2024-05-31 11:53:49 -0600

    finish the image endpoints

---
+++ AppLogic/ImageManager.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Mappers;
+using DTO.Images;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic
+{
+    public class ImageManager
+    {
+        internal ImageMapper mapper = new ImageMapper();
+
+        public void CreateImage(InputImage image)
+        {
+            mapper.CreateImage(image);
+        }
+
+        public List<DbImage> GetAllImages()
+        {
+            return mapper.GetAllImages();
+        }
+
+        public List<DbImage> GetProductImages(int idProduct)
+        {
+            return mapper.GetProductImages(idProduct);
+        }
+
+        public void UpdateImage (DbImage image)
+        {
+            mapper.UpdateImage(image);
+        }
+        public void DeleteImage(int id)
+        {
+            mapper.DeleteImage(id);
+        }
+    }
+}
---
+++ JettMarketApi/Controllers/ImageController.cs
@@ -0,0 +1,83 @@
+﻿using AppLogic;
+using DTO.Images;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JettMarketApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        internal ImageManager manager = new ImageManager();
+
+        [HttpPost]
+        public IActionResult CreateImage(InputImage image) {
+            try
+            {
+                manager.CreateImage(image);
+                return Ok("Image created successfully");
+            }
+            catch (Exception ex) { 
+                return BadRequest(ex.Message);  
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllImages()
+        {
+            try
+            {
+                var result = manager.GetAllImages();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetProductImages(int idProduct)
+        {
+            try
+            {
+                var result = manager.GetProductImages(idProduct);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateImage(DbImage image)
+        {
+            try
+            {
+                manager.UpdateImage(image);
+                return Ok("Image updated successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteImage(int id)
+        {
+            try
+            {
+                manager.DeleteImage(id);
+                return Ok("Image deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
